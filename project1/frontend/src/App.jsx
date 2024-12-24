391ab9ff18adce44194d47e4093315154207659e
Muhammad Tayyab <156538622+TayyabXtreme@users.noreply.github.com>
2024-12-24 18:13:13 +0500

    backend series part1

---
+++ project1/frontend/src/App.jsx
@@ -0,0 +1,39 @@
+
+import { useEffect, useState } from 'react'
+import './App.css'
+import axios from 'axios'
+
+function App() {
+  const [jokes, setJokes] = useState([])
+
+  useEffect(()=>{
+    axios.get('/api/jokes').then((res)=>{
+      setJokes(res.data)
+    }).catch((err)=>{
+      console.log(err)
+    })
+
+
+  },[])
+
+
+  return (
+    <>
+    <h1>Xtrem and full stack</h1>
+    <p>Jokes:{jokes.length}</p>
+
+    {
+      jokes.map(joke=>(
+        <div key={joke.id}>
+          <h3>{joke.title}</h3>
+          <p>{joke.content}</p>
+        </div>
+      ))
+    }
+
+    
+    </>
+  )
+}
+
+export default App
