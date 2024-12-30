c9e0efbed0a9c4b8cb2356169418a8210818a6bd
Muhammad Tayyab <156538622+TayyabXtreme@users.noreply.github.com>
2024-12-30 21:35:45 +0500

    chapter no 3

---
+++ chapterno3/index.js
@@ -0,0 +1,31 @@
+//1. Declare a variable called age & assign to it your age. Show
+//your age in an alert box.
+
+
+//var age=19;
+//alert("I am "+age+" years old");
+
+
+
+//3. Declare a variable called birthYear & assign to it your
+//birth year. Show the following message in your browser:
+
+//var birth="2004";
+//document.write("My birth year is "+birth+"<br>"+"Data type of my declared varialbe is number");
+
+
+/*4. A visitor visits an online clothing store
+www.xyzClothing.com . Write a script to store in variables
+the following information:
+a. Visitor’s name
+b. Product title
+c. Quantity i.e. how many products a visitor wants to
+order
+Show the following message in your browser: “John
+Doe ordered 5 T-shirt(s) on XYZ Clothing store*/
+
+//var userNAme="Jhon Dow";
+//var order=5;
+//var item="T-shirt(s)";
+//var website="XYZ Clothing store";
+//document.write("<b>"+userNAme+"</b>"+ " ordered "+"<b>"+order+" "+item+"</b>"+"on <b>" +website);
