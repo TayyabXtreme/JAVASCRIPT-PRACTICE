3b2cf906850628745ab489fa0874bba57e8911fb
Muhammad Tayyab <156538622+TayyabXtreme@users.noreply.github.com>
2024-12-30 21:34:39 +0500

    chapter no 1

---
+++ chapterno1/index.js
@@ -0,0 +1,38 @@
+//1. Write a script to greet your website visitor using JS alert 
+//box.
+
+//alert("welcome user ");
+
+//2. Write a script to display following message on your web 
+//page:
+
+//alert("Error! Please enter a valid password");
+
+//3. Write a script to display following message on your web 
+//page: (Hint : Use line break)
+
+//alert("Welcome to JS Land...\nHappy Coding!");
+
+//4. Write a script to display following messages in sequence
+
+//alert("Welcome to JS Land...");
+//alert("Happy Coading!\n☐ Prevent this page from creating additional dialogs.");
+
+//5. Generate the following message through browser’s 
+//developer console:
+
+//write this below line in console
+
+//alert("Hello... I can run JS through my Web browser's console");
+
+//6. Make use of alerts in your new/existing HTML & CSS 
+//project.
+
+//  7. Practice placement of <script></script> element in 
+//  following sections of your project in exercise 6:
+//  a. Head
+//  b. Body (before your page’s HTML)
+//  c. Body (inside your page’s HTML)
+//  d. Body (after your page’s HTML
+
+//👉go to the html page and see the comment script
